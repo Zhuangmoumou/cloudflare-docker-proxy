39b0ff0ca4d800d4236bc05e3bf540ab09554216
Zhuangmoumou <103405023+Zhuangmoumou@users.noreply.github.com>
2024-07-19 14:22:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.413121.xyz": dockerHub,
+  "quay.413121.xyz": "https://quay.io",
+  "gcr.413121.xyz": "https://gcr.io",
+  "k8s-gcr.413121.xyz": "https://k8s.gcr.io",
+  "k8s.413121.xyz": "https://registry.k8s.io",
+  "ghcr.413121.xyz": "https://ghcr.io",
+  "cloudsmith.413121.xyz": "https://docker.cloudsmith.io",
+  "ecr.413121.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.413121.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
